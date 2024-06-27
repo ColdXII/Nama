6fa10e29d571c3d63aead502f7b7fef848ed5752
ColdX <Stevengemer@gmail.com>
2024-06-27 09:39:02 +0700

    first commit

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Profile from './profile'
+
+
+render(<Profile/>, document.getElementById('root'))
---
+++ client/profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const Profile = () => {
+    return (
+        <div align="center">
+          <h1>Nama: STEVEN</h1>
+          <h1>NPM: 221510006</h1>
+        </div>
+      )
+}
+
+export default hot(module)(Profile)
